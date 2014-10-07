8fc82d06338d5ace7bf7564062be052bd9391850
giuocob <pentameterpete@gmail.com>
2014-10-06 22:19:33 -0400

    added automated item list generator for soon-to-be cartesian router

---
+++ test/generate-cartesian-itemlist.js
@@ -0,0 +1,45 @@
+// Make a quick 'n simple itemlist out of random cartesian pairs, for use by the cartesian test router.
+// The filename can be given via command line. Resulting item list will be in itemlists/tmp (so we can gitignore it)
+var fs = require('fs');
+var args = require('minimist')(process.argv.slice(2));
+
+function run(cb) {
+	var path = __dirname + '/../itemlists/tmp';
+	var defaults = {
+		filename: 'cartesian.json'
+	};
+	var filename = args._[0] || defaults.filename;
+	if(!fs.existsSync(path)) fs.mkdirSync(path);
+
+	//Uniform distribution from 0 to 100 for now
+	function uniform() {
+		var rnd = Math.random() * 100;
+		return Math.round(rnd * 100) / 100;   // Round to 2 decimal places
+	}
+	var getX = uniform, getY = uniform;
+
+	var itemCount = 100;
+	var itemListMap = {};
+	for(var i=0; i<itemCount; i++) {
+		var item;
+		do {
+			item = {
+				x: getX(),
+				y: getY()
+			};
+			item.id = item.x + '-' + item.y;
+			item.name = '(' + item.x + ', ' + item.y + ')';
+		} while(itemListMap[item.id]);   // Ensure uniqueness
+		itemListMap[item.id] = item;
+	}
+
+	var itemList = [];
+	Object.keys(itemListMap).forEach(function(itemId) {
+		itemList.push(itemListMap[itemId]);
+	});
+	fs.writeFile(path + '/' + filename, JSON.stringify(itemList,null,4), cb);
+}
+run(function(error) {
+	if(error) return console.log(error);
+	console.log('OK');
+});
