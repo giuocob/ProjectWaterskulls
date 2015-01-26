9ae132ada2564a5e4feb85fbf43b73ee8c49bf08
giuocob <pentameterpete@gmail.com>
2015-01-25 21:50:08 -0500

    added some flesh to RIGenerator, and a new cardutils library

--- generators/randomized-iterative.js
+++ generators/randomized-iterative.js
@@ -1,13 +1,34 @@
 var Generator = require('./generator');
 var util = require('util');
+var cardUtils = require('../lib/card-utils');
+
+var defaultBoardSize = 5;
 
 var RIGenerator = function(router) {
 	Generator.call(this, router);
 };
 util.inherits(RIGenerator, Generator);
 
-RIGenerator.prototype.getCard = function(rng) {
-	throw new Error('Randomized iterative generator still being thought about, coming soon :)');
+RIGenerator.prototype.getCard = function(rng, opts) {
+	if(!opts) opts = {};
+	var boardSize = opts.size || defaultBoardSize;
+	if(boardSize < 2 || boardSize > 7) throw new Error('Provided boardSize is out of range');
+	var worker = new Worker(this.router, rng, boardSize);
+	var generated = worker.generate();
+	return generated;
+};
+
+
+// Internal worker class for RIGenerator; does the actual work
+var Worker = function(router, rng, boardSize) {
+	this.router = router;
+	this.rng = rng;
+	this.boardSize = boardSize;
+	this.rowSquareMap = cardUtils.constructRowSquareMap(boardSize);
+};
+
+Worker.prototype.generate = function() {
+	throw new Error('Nothing here yet');
 };
 
 module.exports = RIGenerator;
